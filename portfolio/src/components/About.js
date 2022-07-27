8417b217c4c49a4ec99a60429a2cbb2c61d661b3
Jaclyn Chen <jaclyn.chen5@gmail.com>
2022-07-27 16:59:54 -0400

    changed hover text for navBar

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,17 @@
+const About = () => {
+  return (
+    <div>
+      <h1> About</h1>
+
+      <h1> Jaclyn Chen</h1>
+
+      <p>
+        {" "}
+        Hi! I'm Jaclyn, a student at the University of Washington studying
+        computer science
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -1,31 +1,51 @@
-import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
-import { FaFire, FaPoo } from 'react-icons/fa';
+import { Nav } from "react-bootstrap";
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
+import { FaFire, FaPoo } from "react-icons/fa";
+
+import { HashLink } from "react-router-hash-link";
 
 const NavBar = () => {
   return (
-    <div className="fixed top-0 left-0 h-screen w-16 flex flex-col
-                  bg-white dark:bg-gray-900 shadow-lg">
-                    
-        <SideBarIcon icon={<FaFire size="28" />} />
-        <Divider />
-        <SideBarIcon icon={<BsPlus size="32" />} />
-        <SideBarIcon icon={<BsFillLightningFill size="20" />} />
-        <SideBarIcon icon={<FaPoo size="20" />} />
-        <Divider />
-        <SideBarIcon icon={<BsGearFill size="22" />} />
+    <div
+      className="fixed top-0 left-0 h-screen w-16 flex flex-col
+                  bg-white dark:bg-gray-900 shadow-lg"
+    >
+      <Nav.Link href="#home">
+        <SideBarIcon icon={<FaFire size="28" />} text={"home"} />
+      </Nav.Link>
+      <Divider />
+      <Nav.Link href="#about">
+        <SideBarIcon icon={<BsPlus size="32" />} text={"about"} />
+      </Nav.Link>
+      <Nav.Link href="#education">
+        <SideBarIcon
+          icon={<BsFillLightningFill size="20" />}
+          text={"education"}
+        />
+      </Nav.Link>
+
+      <Nav.Link href="#resume">
+        <SideBarIcon icon={<FaPoo size="20" />} text={"resume"} />
+      </Nav.Link>
+
+      <Nav.Link href="#projects">
+        <SideBarIcon icon={<BsGearFill size="22" />} text={"projects"} />
+      </Nav.Link>
+
+      <Divider />
+
+      {/* light/dark toggle */}
+      <SideBarIcon icon={<BsGearFill size="22" />} />
     </div>
   );
 };
 
-const SideBarIcon = ({ icon, text = 'tooltip 💡' }) => (
+const SideBarIcon = ({ icon, text = "tooltip 💡" }) => (
   <div className="sidebar-icon group">
     {icon}
-    <span class="sidebar-tooltip group-hover:scale-100">
-      {text}
-    </span>
+    <span class="sidebar-tooltip group-hover:scale-100">{text}</span>
   </div>
 );
-
 
 const Divider = () => <hr className="sidebar-hr" />;
 
