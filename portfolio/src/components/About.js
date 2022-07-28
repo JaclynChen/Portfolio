1b0d3f94c8b2987e9dd3225e394aa7e03f8f604b
Jaclyn Chen <jaclyn.chen5@gmail.com>
2022-07-27 21:13:49 -0400

    add social icons

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,3 +1,8 @@
+import {FaGithub, FaLinkedinIn} from "react-icons/fa";
+import {SiDevpost} from "react-icons/si"
+import { Nav } from "react-bootstrap";
+
+
 const About = () => {
   return (
 
@@ -13,13 +18,56 @@ const About = () => {
       <h2> Jaclyn Chen</h2>
 
       <p>
-        Hi! I'm Jaclyn, a student at the University of Washington studying
-        computer science
+        I'm a student at the University of Washington studying
+        computer science.
       </p>
+
+      <div className = "flex space-x-4"> 
+
+      <Nav.Link href="https://www.linkedin.com/in/jaclyn-chen5/">
+        <SideBarIcon icon={<FaLinkedinIn size="28" />}/>
+      </Nav.Link>
+
+      
+      <Nav.Link href="https://github.com/JaclynChen">
+        <SideBarIcon icon={<FaGithub size="28" />}/>
+      </Nav.Link>
+
+      
+      <Nav.Link href="https://devpost.com/jaclynchen5?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav">
+        <SideBarIcon icon={<SiDevpost size="28" />}/>
+      </Nav.Link>
+
+
+
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+      <h3>Skills</h3>
+
+
     </div>
 
     </section>
   );
 };
+
+
+const SideBarIcon = ({ icon}) => (
+    <div className="sidebar-icon group">
+      {icon}
+     
+    </div>
+  );
+  
 
 export default About;
