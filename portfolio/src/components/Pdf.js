0f9f5557c59c740216b2a8214261880ea3a0f58f
Jaclyn Chen <jaclyn.chen5@gmail.com>
2022-07-28 21:49:26 -0400

    get rid of react-pdf element

---
+++ portfolio/src/components/Pdf.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Document, Page,pdfjs } from 'react-pdf';
+import resume_june_22 from "../assets/Jaclyn_Chen_Resume_June22.pdf";
+
+
+const url = 
+"https://cors-anywhere.herokuapp.com/http://www.pdf995.com/samples/pdf.pdf"
+
+const ResumePdf = () =>{
+
+    pdfjs.GlobalWorkerOptions.workerSrc = 
+    `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+    return (
+        <>
+        <div className="main">
+          <Document
+            file={resume_june_22}
+            //onLoadSuccess={onDocumentLoadSuccess}
+            >
+            <Page pageNumber={1} />
+          </Document>
+         </div>
+        </>
+      );
+
+
+}
+
+export default ResumePdf;
--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -1,30 +1,22 @@
-import Jaclyn_Chen_Resume_June22 from "../assets/Jaclyn_Chen_Resume_June22.pdf";
-
 import React from "react";
-import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import Button from "react-bootstrap/Button";
+import Jaclyn_Chen_Resume_June22 from "../assets/Jaclyn_Chen_Resume_June22.pdf";
+import ResumePdf from "./Pdf";
 
 const Resume = () => {
   return (
     <section className="about" id="resume">
-      <div className="mt-20">
-        <h1>Resume</h1>
+      <h1>Resume</h1>
 
-        <a
-          href={Jaclyn_Chen_Resume_June22}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button className = "custom-button">
-          Open and download pdf
+      <a
+        href={Jaclyn_Chen_Resume_June22}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Button className="custom-button">Open and download pdf</Button>
+      </a>
 
-          </Button>
-        </a>
-        
-        <Document file={Jaclyn_Chen_Resume_June22}>
-          <Page pageNumber={1} />
-        </Document>
-      </div>
+      {/* <ResumePdf /> */}
     </section>
   );
 };
