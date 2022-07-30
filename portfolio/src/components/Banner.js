fc373675d8adee7858f0a16efde38ab88051de6f
Jaclyn Chen <jaclyn.chen5@gmail.com>
2022-07-30 16:49:49 -0400

    deleted imports

--- portfolio/src/components/Banner.js
+++ portfolio/src/components/Banner.js
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-//import headerImg from "../assets/img/header-img.svg";
-//import { ArrowRightCircle } from "react-bootstrap-icons";
-//import sky from "../assets/img/sky.jpg"
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 
