eb146be05ac7a37489a4ae0e8401ebaee0d08162
Jaclyn Chen <jaclyn.chen5@gmail.com>
2022-07-30 22:08:55 -0400

    fixed tag positioning, bottom and wrap around

--- portfolio/src/templates/projectTemplate.js
+++ portfolio/src/templates/projectTemplate.js
@@ -1,46 +1,27 @@
 import React from "react";
-import {Button, Card, Nav, Badge} from "react-bootstrap";
+import { Button, Card, Nav, Badge } from "react-bootstrap";
 //import 'bootstrap/dist/css/bootstrap.css';
 
+const ProjectCard = ({ title, description, link, tags }) => {
+  return (
+    <Nav.Link href={link}>
+      <div className="projectCard">
+        <h3 className="my-4">{title}</h3>
 
-const ProjectCard = ({title, description, link, tags}) =>{
+        <p className="m-0">{description}</p>
 
-    return (
-
-      <Nav.Link href= {link}>
-
-      <div className="projectCard"> 
-      
-
-      <h3 className="my-4">{title}</h3>
-
-      <p className="m-0">{description}</p>
-
-
-
-      <div className=" mx-4 bottom-1 flex items-end  grid grid-cols-3"> 
-       
-       {tags.map((tagTitle, index) => {
-
-        return <Badge key={index} className="badge">{tagTitle}</Badge>;
-
-       })}
-
-        </div> 
-
-
+        <div className=" mx-4 absolute bottom-0 flex flex-wrap pb-4">
+          {tags.map((tagTitle, index) => {
+            return (
+              <div key={index} className="badge">
+                {tagTitle}
+              </div>
+            );
+          })}
+        </div>
       </div>
+    </Nav.Link>
+  );
+};
 
-      </Nav.Link>
-        
-
-    
-      );
-
-
-
-
-
-}
-
-export default ProjectCard; 
+export default ProjectCard;
