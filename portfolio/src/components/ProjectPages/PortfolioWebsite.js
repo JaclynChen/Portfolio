6a61676ee8452dbee9b419c0f7ec9c37ad938f10
Jaclyn Chen <jaclyn.chen5@gmail.com>
2022-07-30 21:23:22 -0400

    populated tags to projects

---
+++ portfolio/src/components/ProjectPages/PortfolioWebsite.js
@@ -0,0 +1,14 @@
+const PortfolioWebsite = () =>{
+    return(
+  
+      <div id="PortfolioWebsite">
+  
+        <p>hi</p>
+      </div>
+  
+    );
+  
+  
+  }
+
+  export default PortfolioWebsite; 
